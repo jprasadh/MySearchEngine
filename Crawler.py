fb72ef51f28ea86806c9e3ddea07afcd54f48ca8
Jai Prasadh <jai.g.prasadh@gmail.com>
2018-01-01 22:16:32 -0600

    New file

--- Crawler.py
+++ Crawler.py
@@ -22,9 +22,31 @@ def get_all_links(page):
     return links
 
 
-def get_source(site):                                   # takes in a url and outputs html source for crawling purposes
-    page = 0
-    return page
+def get_source(url):                                   # takes in a url and outputs html source for crawling purposes
+    try:
+        if url == "http://www.udacity.com/cs101x/index.html":
+            return ('<html> <body> This is a test page for learning to crawl! '
+                    '<p> It is a good idea to '
+                    '<a href="http://www.udacity.com/cs101x/crawling.html">learn to '
+                    'crawl</a> before you try to  '
+                    '<a href="http://www.udacity.com/cs101x/walking.html">walk</a> '
+                    'or  <a href="http://www.udacity.com/cs101x/flying.html">fly</a>. '
+                    '</p> </body> </html> ')
+        elif url == "http://www.udacity.com/cs101x/crawling.html":
+            return ('<html> <body> I have not learned to crawl yet, but I '
+                    'am quite good at '
+                    '<a href="http://www.udacity.com/cs101x/kicking.html">kicking</a>.'
+                    '</body> </html>')
+        elif url == "http://www.udacity.com/cs101x/walking.html":
+            return ('<html> <body> I cant get enough '
+                    '<a href="http://www.udacity.com/cs101x/index.html">crawling</a>! '
+                    '</body> </html>')
+        elif url == "http://www.udacity.com/cs101x/flying.html":
+            return ('<html> <body> The magic words are Squeamish Ossifrage! '
+                    '</body> </html>')
+    except:
+        return ""
+    return ""
 
 
 def union(p, q):
@@ -33,18 +55,21 @@ def union(p, q):
             p.append(e)
 
 
-def crawl(seed):
+def crawl(seed, max_pages):
     tocrawl = [seed]                                    # to-do list for crawler
     crawled = []                                        # output of crawler: list of crawled urls
     while tocrawl:
-        url = tocrawl.pop(0)                            # crawls starting from *first* page in list "tocrawl"
-        if url not in crawled:                          # ensures that we do not crawl the same site over and over again
-            links = get_all_links(get_source(url))      # crawls last page and stores in "links"
-            union(tocrawl, links)                       # adds newly found links to "tocrawl" using union method
-            crawled.append(url)                         # appends crawled page to "crawled"
+        if len(crawled) < max_pages:
+            url = tocrawl.pop(0)                            # crawls starting from *first* page in list "tocrawl"
+            if url not in crawled:                          # ensures that we do not crawl the same site over and over again
+                links = get_all_links(get_source(url))      # crawls last page and stores in "links"
+                union(tocrawl, links)                       # adds newly found links to "tocrawl" using union method
+                crawled.append(url)                         # appends crawled page to "crawled"
+        else:
+            break
     return crawled
 
 
 # seed page is just some url with links on it
 seed_site = "http://webtrain.austincc.edu/~jprasadh/"
-print(crawl(seed_site))
+print(crawl(seed_site, 500))
