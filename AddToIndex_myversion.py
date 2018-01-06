8491dc1dcee0854da5ddc4624a1e441fcbd5da1a
Jai Prasadh <jai.g.prasadh@gmail.com>
2018-01-05 23:14:13 -0600

    New file

---
+++ AddToIndex_myversion.py
@@ -0,0 +1,42 @@
+# Define a procedure, add_to_index,
+# that takes 3 inputs:
+
+# - an index: [[<keyword>,[<url>,...]],...]
+# - a keyword: String
+# - a url: String
+
+# If the keyword is already
+# in the index, add the url
+# to the list of urls associated
+# with that keyword.
+
+# If the keyword is not in the index,
+# add an entry to the index: [keyword,[url]]
+
+index = []
+
+
+def keyword_present(index, keyword):
+    for entry in index:
+        i = 0
+        if entry[0] == keyword:
+            return i
+        i += 1
+    return -1
+
+def add_to_index(index, keyword, url):
+    if keyword_present(index, keyword) == -1:
+        index.append([keyword, [url]])
+    else:
+        index[keyword_present(index, keyword)][1].append(url)
+
+
+
+add_to_index(index,'udacity','http://udacity.com')
+add_to_index(index,'computing','http://acm.org')
+add_to_index(index,'udacity','http://npr.org')
+print(index)
+#>>> [['udacity', ['http://udacity.com', 'http://npr.org']],
+#>>> ['computing', ['http://acm.org']]]
+
+
